e08b617b5f1365a34726e4d3749d87d02864c4a5
Jang_Desktop <sdsd090811@gmail.com>
2022-08-17 10:31:43 +0900

    Add 스프링 기본 설정

---
+++ ObjectRestaurant/src/main/java/com/object/objectrestaurant/ObjectRestaurantApplication.java
@@ -0,0 +1,13 @@
+package com.object.objectrestaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObjectRestaurantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObjectRestaurantApplication.class, args);
+	}
+
+}
---
+++ ObjectRestaurant/src/test/java/com/object/objectrestaurant/ObjectRestaurantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.object.objectrestaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObjectRestaurantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
