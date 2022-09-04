77727d2577494557b75c4799e3ad28d58bf1d4a9
Jang_Desktop <sdsd090811@gmail.com>
2022-09-04 15:49:40 +0900

    Add 아주 기본적인 예외처리 핸들러 작성

---
+++ ObjectRestaurant/src/main/java/com/object/objectrestaurant/exception/GlobalExceptionHandler.java
@@ -0,0 +1,17 @@
+package com.object.objectrestaurant.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@ControllerAdvice
+public class GlobalExceptionHandler {
+	
+	@ExceptionHandler(value=IllegalArgumentException.class)
+	public String handleArgumentException(IllegalArgumentException e) {
+		return "<h1>"+e.getMessage()+"<h1>";
+	}
+	
+}
